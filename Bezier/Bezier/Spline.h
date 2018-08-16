74069659e90164301f4dac1b671bbe57a383b7d7
mrgoldtech <cbb369@gmail.com>
2018-08-16 17:42:40 -0500

    Just a test commit... added header file for the "list of Bezier Curves" wrapper class
    Signed-off-by: mrgoldtech <cbb369@gmail.com>

---
+++ Bezier/Bezier/Spline.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include "Bezier.h"
+
+using namespace std;
+
+class Spline {
+
+	/*The wrapper class for a list of Bezier curves
+	Functionality includes: 
+	1st derivative checking at intersection points
+	2nd derivative checking at intersection points
+	Manipulation of points while maintaining localism*/
+
+	Spline(vector<Bezier> curves); //Initialize with a list of defined bezier curves
+
+
+};
